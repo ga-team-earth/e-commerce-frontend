06b191b2751edef9686b8d98944bb88debd16492
ksouth <katesoutherland8@gmail.com>
2022-08-05 17:02:06 -0700

    Added state and carousel

--- src/routes/shop/TitleTwo.js
+++ src/routes/shop/TitleTwo.js
@@ -1,47 +1,37 @@
 import React from 'react'
-import "./Shop.scss"
-import { useState, useEffect } from 'react'
+import "./Shop"
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
+import {motion} from "framer-motion";
 
 const TitleTwo = () => {
 
   const [artItems, setArtItems] = useState([])
   const [titleState, setTitleState] = useState([])
+  const [widthTwo, setWidthTwo] = useState(0);
 
-  useEffect(() => {
-    axios.get('https://e-commerce-earth.herokuapp.com/api/art')
-      .then(res => {
-        let data = res.data
-        let titleTwo = data[1]
-        setArtItems(titleTwo.items)
-        setTitleState(titleTwo)
-
-      })
-  }, [])
 
   return (
-
     <>
-      <div className='test'>
-      {/* Look into multiple components for displaying the data */}
-
-      <ul className='titleTwo'>
-      <h1>{titleState.title}</h1>
+        <h1 className='title'>{titleState.title}</h1>
+        <motion.div ref={carouselTwo}  whileTap={{cursor: "grabbing"}} className='box'>
+        <motion.div drag="x" dragConstraints={{right: 0, left: -widthTwo}} className="inner-carousel-two">
+        <div className='item-two'>
         {artItems.map((artItem, index) => (
-          <>
-            <ul key={index}>
-              {artItem.name}
-              {/* <img src={artItem.imageUrl} alt='blah'></img> */}
-            </ul>
-          </>
-        )
-        )}
-      </ul>
-
-      </div>
-    </>
-
+        <div className='product-card-container'>
+            <img src={artItem.imageUrl} alt="flowers" />
+            <div className='footer'>
+              <span className='name' key={index}>{artItem.name}</span>
+              <span className='price'>{artItem.price}</span>
+              <button>Add to Cart</button>
+            </div>
+        </div>
+      )
+    )}
+    </div>
+    </motion.div>
+    </motion.div>
+  </>
   )
 }
-
 export default TitleTwo
