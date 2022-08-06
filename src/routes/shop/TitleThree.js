c62996e34028fd13dc71aa3aa049424fa0ed6804
ksouth <katesoutherland8@gmail.com>
2022-08-05 17:43:37 -0700

    Edited structure

--- src/routes/shop/TitleThree.js
+++ src/routes/shop/TitleThree.js
@@ -1,46 +1,33 @@
 import React from 'react'
 import "./Shop.scss"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
+import {motion} from "framer-motion";
 
 const TitleThree= () => {
 
-  const [artItems, setArtItems] = useState([])
-  const [titleState, setTitleState] = useState([])
-
-  useEffect(() => {
-    axios.get('https://e-commerce-earth.herokuapp.com/api/art')
-      .then(res => {
-        let data = res.data
-        let titleThree = data[2]
-        setArtItems(titleThree.items)
-        setTitleState(titleThree)
-      })
-  }, [])
-
   return (
-
     <>
-      <div className='test'>
-      {/* Look into multiple components for displaying the data */}
-
-      <ul className='titleThree'>
-      <h1>{titleState.title}</h1>
-        {artItems.map((artItem, index) => (
-          <>
-            <ul key={index}>
-              {artItem.name}
-              {/* <img src={artItem.imageUrl} alt='blah'></img> */}
-            </ul>
-          </>
-        )
-        )}
-      </ul>
-
-      </div>
-    </>
-
+    <h1 className='title'>{titleState.title}</h1>
+    <motion.div ref={carouselTwo}  whileTap={{cursor: "grabbing"}} className='box'>
+    <motion.div drag="x" dragConstraints={{right: 0, left: -widthTwo}} className="inner-carousel-two">
+    <div className='item-two'>
+    {artItems.map((artItem, index) => (
+    <div className='product-card-container'>
+        <img src={artItem.imageUrl} alt="flowers" />
+        <div className='footer'>
+          <span className='name' key={index}>{artItem.name}</span>
+          <span className='price'>{artItem.price}</span>
+          <button>Add to Cart</button>
+        </div>
+    </div>
   )
+)}
+</div>
+</motion.div>
+</motion.div>
+</>
+)
 }
 
 export default TitleThree
